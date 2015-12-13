3d977b479e7c90fab178eeaf506e0f043d922245
Martin Fracker <martin.frackerjr@gmail.com>
2015-12-13 15:17:11 -0600

    Add terminal_clear, terminal_linefeed

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -10,20 +10,26 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
+void terminal_clear()
+{
+	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
+    {
+      for ( size_t x = 0; x < VGA_WIDTH; x++ )
+        {
+          const size_t index = y * VGA_WIDTH + x;
+          terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+        }
+    }
+}
+
 void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
 	terminal_buffer = VGA_MEMORY;
-	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
-	{
-		for ( size_t x = 0; x < VGA_WIDTH; x++ )
-		{
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
-		}
-	}
+
+  terminal_clear();
 }
 
 void terminal_setcolor(uint8_t color)
@@ -33,12 +39,23 @@ void terminal_setcolor(uint8_t color)
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = make_vgaentry(c, color);
+  const size_t index = y * VGA_WIDTH + x;
+  terminal_buffer[index] = make_vgaentry(c, color);
+}
+
+void terminal_linefeed()
+{
+  terminal_row++;
+  terminal_column = 0;
 }
 
 void terminal_putchar(char c)
 {
+  if ( c == '\n' )
+  {
+    terminal_linefeed();
+    return;
+  }
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if ( ++terminal_column == VGA_WIDTH )
 	{
