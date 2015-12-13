0f26bce309782d9f7bbf38750f32742c18231227
Martin Fracker <martin.frackerjr@gmail.com>
2015-12-12 21:44:00 -0600

    Change myos to frackos

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/frackos.kernel isodir/boot/frackos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "frackos" {
+	multiboot /boot/frackos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -d /usr/lib/grub/i386-pc/ -o frackos.iso isodir
