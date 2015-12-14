59883fc18d28a0cc027459da51c5eb3e62e5e657
Martin Fracker <martin.frackerjr@gmail.com>
2015-12-13 23:14:13 -0600

    Create and initialize global descriptor table

---
+++ kernel/include/kernel/gdt.h
@@ -0,0 +1,6 @@
+#ifndef _KERNEL_GDT_
+#define _KERNEL_GDT_
+
+void init_gdt();
+
+#endif
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,8 +4,12 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/gdt.h>
 
-void kernel_early(void) { terminal_initialize(); }
+void kernel_early(void) {
+  // init_gdt();
+  terminal_initialize();
+}
 
 void kernel_main(void) {
   printf("%s\n%s\n", "Welcome to FrackOS!", "Still in development...");
